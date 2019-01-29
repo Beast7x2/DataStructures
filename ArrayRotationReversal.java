ed82d453d0f52f773cf042698e51ecf8b70fea1c
Beast7x2 <arunkumar7@outlook.com>
2019-01-29 16:45:49 +0530

    Add files via upload

---
+++ ArrayRotationReversal.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class ArrayRotationReversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the Array size: ");
+		
+		int arrLen = in.nextInt();
+		
+		int[] inputArray = new int[arrLen];
+		
+		System.out.println("Enter the Array rotation: ");
+		
+		int rotation = in.nextInt();
+		
+		System.out.println("Enter the Array elements: ");
+		
+		for(int i=0; i<arrLen; i++) {
+			inputArray[i] = in.nextInt();
+		}
+		in.close();
+		
+		arrRotation(inputArray,0,rotation-1);
+			
+		arrRotation(inputArray,rotation,arrLen-1);
+			
+		arrRotation(inputArray,0,arrLen-1);
+		
+		
+		for(int i=0; i<arrLen; i++) {
+			System.out.println(inputArray[i]);
+		}
+	}
+	
+	public static void arrRotation(int[] arr, int start, int end) {
+		
+		for(int i=start,j=end; i<j; i++,j--) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+
+}
